e4ad58f0430477bf3cfb19a5c319a1258a190389
Macallan Brown <macallan@perabrown.com>
2017-06-17 13:20:52 -0700

    Setup the firebase database and html/javascript for content page
    The idea for the content page is that it will be a live updating page of things that I like. I am going to create a native mobile app that can write to the database to push changes.

---
+++ contentScript.js
@@ -0,0 +1,54 @@
+
+var isUsingRealData = false; //SET TO FALSE WHILE TESTING TO USE SAMPLE DATA INSTEAD OF CALLING TO DB
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDLSTQgLQA0WTU3r5B4uhex7MPU2HiRszY",
+  authDomain: "personal-website-39df9.firebaseapp.com",
+  databaseURL: "https://personal-website-39df9.firebaseio.com",
+  projectId: "personal-website-39df9",
+  storageBucket: "personal-website-39df9.appspot.com",
+  messagingSenderId: "687288860936"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var movieData;
+var seenMovies;
+
+if(isUsingRealData){
+  var moviesDatabase = firebase.database().ref('data/movies');
+  moviesDatabase.on('value', function(snapshot) {
+    movieData = snapshot.val();
+    movieDataLoaded();
+  });
+} else {
+  $.getJSON("/sampleData/sampleData1.json", function(json) {
+      console.log("------FAKE DATA-------");
+      movieData = json["movies"];
+      movieDataLoaded();
+  });
+}
+
+var movieDataLoaded = function() {
+  seenMovies = movieData["seen-movies"];
+  for (var movie in seenMovies) {
+    $favoriteMovie = jQuery('<tr/>', {
+      class: 'favoriteMovie',
+      align: 'right'
+    });
+
+    $movieTitle = jQuery('<td/>', {
+      class: 'inlineBlock movieTitle',
+      text: movie
+    }).appendTo($favoriteMovie);
+
+    $movieRating = jQuery('<td/>', {
+      class: 'inlineBlock movieRating',
+      text: seenMovies[movie]
+    }).appendTo($favoriteMovie);
+
+    $favoriteMovie.appendTo('#favoriteMovieContainer');
+    console.log(seenMovies[movie]);
+  }
+}
